63b379e0bcf0f9666104a8ec9d300041d9fec340
Kouvousis <dexous1996@gmail.com>
2025-06-08 19:08:33 +0300

    feat: Custom background logic depending on weather conditions

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,47 @@
+import type { WeatherData } from "../api/API";
 import Header from "./Header";
 
 interface LayoutProps {
   children?: React.ReactNode;
+  weatherData?: WeatherData | null;
 }
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({ children, weatherData }: LayoutProps) => {
+  const getBackground = () => {
+    if (!weatherData) {
+      return "default-background";
+    }
+
+    const weatherMain = weatherData.weather[0].main.toLowerCase();
+
+    switch (weatherMain) {
+      case "atmosphere":
+        return "atmosphere-background";
+      case "clear":
+        return "clear-background";
+      case "clouds":
+        return "clouds-background";
+      case "drizzle":
+        return "drizzle-background";
+      case "rain":
+        return "rain-background";
+      case "snow":
+        return "snow-background";
+      case "thunderstorm":
+        return "thunderstorm-background";
+      default:
+        return "default-background";
+    }
+  };
+  const currentBackground = getBackground();
   return (
     <>
       <Header />
-      <div className="container mx-auto min-h-[95vh] pt-24">{children}</div>
+      <div
+        className={`container mx-auto min-h-[100vh] pt-24 ${currentBackground}`}
+      >
+        {children}
+      </div>
     </>
   );
 };
