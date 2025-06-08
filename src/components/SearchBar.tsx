70a2a41fd802f6664b0ded7ec9dd698b3e884a0b
Kouvousis <dexous1996@gmail.com>
2025-06-08 19:19:16 +0300

    style: minor style changes

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -31,7 +31,7 @@ const SearchBar = ({ onSearch }: SearchBarProps) => {
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
             placeholder="Search for a city, country, or place"
-            className="bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 px-2 py-2 w-full"
+            className="bg-gray-800 text-white placeholder-gray-400 focus:outline-none px-2 py-2 w-full"
           />
           <button className="px-4 py-2 bg-gray-800 hover:bg-gray-600 text-white rounded-md">
             <Search />
