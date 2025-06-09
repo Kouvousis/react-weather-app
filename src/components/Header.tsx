03919705500654a136db1edf27d7847ccb733077
Kouvousis <dexous1996@gmail.com>
2025-06-09 17:54:59 +0300

    style: Header now is displayed above all other elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <>
-      <header className="bg-gray-800 fixed w-full">
+      <header className="bg-gray-800 fixed w-full z-50">
         <div className="container mx-auto flex items-center justify-center h-8">
           <span className="text-white text-lg font-semibold flex items-center gap-2">
             Weather App
